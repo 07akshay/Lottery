e4b8fa066704613a165f8797496c26fad6fed641
Akshay Kumar Singh <72185763+07akshay@users.noreply.github.com>
2021-11-23 17:47:17 +0530

    Update allottToken.py

--- Client/allottToken.py
+++ Client/allottToken.py
@@ -9,22 +9,19 @@ app = Flask(__name__)
 def fetch():
     if request.method == 'POST':
         details = request.form
-        userID = details['key']  # use as userid for now
-        contest = details['values']    # contest name
-        
-        token = Token()
-        token.createToken(contest,userID)
+        userID = details['user_id']  # userid
+        contestID = details['contest_id']    # contest ID
         er = Error()
-        if (not checkUser(userID, contest)):
-            er.userExists()        
-        if (not tokenAvailable(token)):
-            er.tokenNotAvailable()
+        if (not checkUser(userID, contestID)):
+            er.userExists()
         if er.errorCode is not None:
-            return er.error
-        
-        insertToDB(token)
-        return token.Number
-    return render_template("new.html")
+            return render_template("getToken.html",msg = er.error)
+        else:
+            token = Token()
+            token.createToken(contestID,userID)
+            insertToDB(token)
+            return token.Number
+    return render_template("getToken.html")
 
 if __name__=='__main__':
     app.run(host='127.0.0.1',port = '5055')
